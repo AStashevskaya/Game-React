a2bc3629965de3271dc764e791d07a90e9c62179
AStashevskaya <stashevskayaaa@gmail.com>
2021-03-01 22:01:41 +0300

    feat: add scores page

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useSelector } from 'react-redux';
 // import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
 import MenuButton from '../Menu/MenuButton';
+import SmallButton from '../Menu/options/SmallButton';
 import { menuLink } from '../../data/navBarData';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,13 +21,47 @@ const useStyles = makeStyles((theme) => ({
 
 const GameOver = () => {
   const score = localStorage.getItem('final-score');
-
-  console.log(score);
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+  const scores = JSON.parse(localStorage.getItem('scores')) || [];
   const { path, title } = menuLink;
 
   const classes = useStyles();
-  const handleChange = (e) => {
-    console.log(e);
+  // const handleChange = ({ target: value }) => {
+  //   setName(name + value);
+  // };
+
+  const handleChange = ({ target }) => {
+    setName(target.value);
+  };
+
+  const getDate = () => {
+    const newDate = new Date();
+    const day = newDate.getDate();
+    const month = newDate.getMonth();
+    const year = newDate.getFullYear();
+
+    setDate(`${day}/${month + 1}/${year}`);
+  };
+
+  useEffect(() => {
+    getDate();
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const obj = {
+      name, score, date,
+    };
+    scores.push(obj);
+    localStorage.setItem('scores', JSON.stringify(scores));
+    setName('');
+    document.location.replace('/');
+  };
+
+  const handleKeyPress = (e) => {
+    const { key } = e;
+    if (key === 'Enter') handleClick(e);
   };
 
   return (
@@ -37,8 +72,13 @@ const GameOver = () => {
         {JSON.parse(score)}
       </h2>
       <FormControl variant="filled" className={classes.formControl}>
-        <Input id="demo-simple-select-filled-label" value="" onChange={handleChange} />
+        <Input id="demo-simple-select-filled-label" value={name} onChange={handleChange} onKeyPress={handleKeyPress} />
+        <SmallButton text="send" handleClick={handleClick} submit={handleKeyPress} />
       </FormControl>
+      {/* <form>
+        <input value={name} onChange={handleChange} />
+        <SmallButton text="send" handleClick={handleClick} />
+      </form> */}
       <MenuButton
         text={title}
         path={path}
--- src/components/Menu/Scores.jsx
+++ src/components/Menu/Scores.jsx
@@ -5,10 +5,37 @@ import MenuButton from './MenuButton';
 
 const ScoresPage = () => {
   const { path, title } = menuLink;
+  let results = JSON.parse(localStorage.getItem('scores'));
+  results = results.sort((a, b) => b.score - a.score);
+  results = results.length <= 10 ? results : results.slice(0, 10);
 
   return (
-    <div className="options">
-      <h1> scores </h1>
+    <div className="score">
+      <div className="score__title">
+        <h2> scores </h2>
+      </div>
+      <div className="score__content">
+        <table className="score__table">
+          <tr className="score__heading">
+            <td>№</td>
+            {' '}
+            <td>Name</td>
+            {' '}
+            <td>Date</td>
+            {' '}
+            <td>Score</td>
+          </tr>
+          {results.map((row, idx) => (
+            <tr>
+              <td>{idx + 1}</td>
+              <td>{row.name}</td>
+              <td>{row.date}</td>
+              <td>{row.score}</td>
+            </tr>
+          ))}
+
+        </table>
+      </div>
 
       <MenuButton
         text={title}
